7a42bd7602467802bf5ad0bf54be85e9db14fca6
Yzabakli <abakliyusufziya@gmail.com>
2022-08-20 20:37:51 +0200

    All interview tasks are completed

--- src/number_tasks/Number_BinaryRepresentationOfN.java
+++ src/number_tasks/Number_BinaryRepresentationOfN.java
@@ -1,19 +1,35 @@
 package number_tasks;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Number_BinaryRepresentationOfN {
     public static void main(String[] args) {
 
-        System.out.println(getCountOfOnes2(464347383));
-
+        long start = System.currentTimeMillis();
+        System.out.println(getCountOfOnes2(16777216));
+        System.out.println("elapsed time = " + (System.currentTimeMillis() - start));
     }
     private static int getCountOfOnes(int n) {
 
         return Collections.frequency(List.of(Integer.toBinaryString(n).split("")), "1");
     }
-    private static int getCountOfOnes2(int n) {
+    private static int getCountOfOnes2(int n){
+
+        int count = 0;
+
+        while (n > 0){
+
+            if (n % 2 != 0){
+
+                count++;
+            }
+            n /= 2;
+        }
+        return count;
+    }
+    private static int solution(int n) {
 
         String s = "";
         boolean result = n > 0;
