1628d836661487d6c5c0ab6151732c025e4d7a49
Yzabakli <abakliyusufziya@gmail.com>
2022-08-20 14:27:23 +0200

    All interview tasks are completed

--- src/number_tasks/Number_Fibonacci.java
+++ src/number_tasks/Number_Fibonacci.java
@@ -3,7 +3,7 @@ package number_tasks;
 public class Number_Fibonacci {
     public static void main(String[] args) {
 
-        System.out.println(isFibonacci(10));
+        System.out.println(isFibonacci(2));
 
     }
     private static long isFibonacci(int n){
@@ -11,7 +11,7 @@ public class Number_Fibonacci {
         int j = 0;
         int z = 1;
 
-        for (int i = 2; i < n; i++) {
+        for (int i = 0; i < n; i++) {
 
             z += j;
 
