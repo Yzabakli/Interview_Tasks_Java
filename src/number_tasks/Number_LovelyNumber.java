bec6aca257696df26726b8630a8ac1a5522c971d
Yzabakli <abakliyusufziya@gmail.com>
2022-08-21 16:07:40 +0200

    All interview tasks are completed

--- src/number_tasks/Number_LovelyNumber.java
+++ src/number_tasks/Number_LovelyNumber.java
@@ -1,9 +1,6 @@
 package number_tasks;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Stream;
 
 import static java.util.Collections.frequency;
@@ -12,12 +9,11 @@ public class Number_LovelyNumber {
     public static void main(String[] args) {
 
         long start = System.currentTimeMillis();
-        System.out.println(countLovelyNumbers(1, 4879));
+        System.out.println(lovelyNumber(1, 4789));
         System.out.println("elapsed time = " + (System.currentTimeMillis() - start));
 
         start = System.currentTimeMillis();
-        System.out.println(lovelyNumber(1099, 4879));
-        System.out.println(lovelyNumber(5, 110));
+        System.out.println(solutionWithMapAndStream(1, 4789));
         System.out.println("elapsed time = " + (System.currentTimeMillis() - start));
     }
 
@@ -60,23 +56,70 @@ public class Number_LovelyNumber {
         return count;
 
     }
-    public static int countLovelyNumbers(int a, int b){
-        int count = 0;
-        for (int i = a; i <= b; i++) {
-            List<String> list = new ArrayList<>(Arrays.asList((i+"").split("")));
-            boolean result = false;
 
-            for (String each : list) {
-                if (frequency(list, each) < 3) {
-                    result = true;
-                    break;
+    public static int solutionWithNestedFor(int a, int b){
+        int lovelyNumberCount = 0;
+
+        //looping from A to B (both ends included)
+        for (int i = a; i <= b; i++) {
+            //getting all digits in the number as String List
+            List<String> list = new ArrayList<>(Arrays.asList((i+"").split("")));
+
+            boolean isNumberLovely = false;
+
+            for (String eachDigit : list) {
+                if(Collections.frequency(list, eachDigit) < 3)
+                    isNumberLovely = true;
+                else{
+                    isNumberLovely = false;
+                    break;//if we find a digit is occurring more than 2 times, then, no need to check rest. the number is not lovely number.
                 }
             }
-            if(result) {
-                count++;
+            if(isNumberLovely) {
+                lovelyNumberCount++;
             }
         }
 
-        return count;
+        return lovelyNumberCount;
+    }
+
+    public static int solutionWithMapAndStream(int a, int b){
+        int lovelyNumberCount = 0;
+        Map<Character, Integer> digitFrequencies;
+
+        if (a < 1000){
+
+            lovelyNumberCount = 1000 - a;
+
+            if (b > 1000){
+                lovelyNumberCount -= 9 - (a / 111);
+
+            }else {
+                return lovelyNumberCount - ((b / 111) - (a / 111));
+            }
+            a = 1000;
+        }
+
+
+        while (a <= b){
+            digitFrequencies = new HashMap<>();
+            char[] digitsAsCharArray = String.valueOf(a).toCharArray();
+
+            for (char c : digitsAsCharArray) {
+                digitFrequencies.put(c, digitFrequencies.getOrDefault(c, 0) + 1);
+
+                //if the current digit occurs more than two, then no need to check other digits.
+                if (digitFrequencies.getOrDefault(c, 0) > 2)
+                    break;
+            }
+
+            //checking if there is any digit that has more than two frequencies.
+            if (digitFrequencies.values().stream().noneMatch(frequency -> frequency > 2))
+                lovelyNumberCount++;
+
+            a++;
+        }
+
+        return lovelyNumberCount;
     }
 }
