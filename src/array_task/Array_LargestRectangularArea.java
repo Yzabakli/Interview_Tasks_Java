9b0d1360fa734630927b08cf87b22e00596708dc
Yzabakli <abakliyusufziya@gmail.com>
2022-08-26 18:25:53 +0200

    All interview tasks are completed

--- src/array_task/Array_LargestRectangularArea.java
+++ src/array_task/Array_LargestRectangularArea.java
@@ -13,7 +13,7 @@ public class Array_LargestRectangularArea {
 
     private static int largestRectangularArea(int[] array) {
 
-        List<Integer> list = new ArrayList<>();
+        int max = 0;
 
         for (int i = 0; i < array.length; i++) {
             int count = 1;
@@ -33,8 +33,8 @@ public class Array_LargestRectangularArea {
                 }
                 count++;
             }
-            list.add(array[i] * count);
+            max = Math.max(max, array[i] * count);
         }
-        return Collections.max(list);
+        return max;
     }
 }
