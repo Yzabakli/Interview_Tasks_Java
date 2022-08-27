f5f3100906f4e56e573c93a266c7af4d1cd1980c
Yzabakli <abakliyusufziya@gmail.com>
2022-08-27 16:53:12 +0200

    All interview tasks are completed

--- src/array_task/Array_SortInAscendingOrder.java
+++ src/array_task/Array_SortInAscendingOrder.java
@@ -6,45 +6,40 @@ public class Array_SortInAscendingOrder {
     public static void main(String[] args) {
 
         System.out.println(Arrays.toString(sortInAscendingOrder(new int[]{5, 5, 5, 5, 5, 4, 3, 2, 1, 5})));
-
     }
 
     private static int[] sortInAscendingOrder(int[] array) {
 
-        int[] numbers = new int[array.length];
-
+        int[] numbers = Arrays.copyOf(array, array.length);
         for (int i = 0; i < array.length; i++) {
-            int j = 0, m = 0, n = 0;
-            numbers[i] = Integer.MAX_VALUE;
-            for (int number : array) {
-                if (number < numbers[i]) {
-                    j++;
-                    numbers[i] = number;
-                } else n++;
-                m++;
+            int index = 0;
+            for (int j = i + 1; j < array.length; j++) {
+
+                if (array[j] < array[i]){
+
+                    numbers[i] = array[j];
+                    numbers[j] = array[i];
+                    index = j;
+                }
             }
-            array[array.length - (m - j)] = Integer.MAX_VALUE;
+            array[i] = numbers[i];
+            array[index] = numbers[index];
         }
         return numbers;
     }
 
-    public static int[] sortingArrayAsc(int[] arr) {
+    private static int[] sortInAscendingOrder2(int[] array) {
 
-        int[] result = Arrays.copyOfRange(arr, 0, arr.length);
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
 
-        for (int i = 0; i < result.length; i++) {
-
-            for (int j = 0; j < result.length; j++) {
-                if (result[i] < result[j]) {
-                    Integer temp = result[i];
-                    result[i] = result[j];
-                    result[j] = temp;
+                if (array[j] < array[i]){
+                    int temp = array[j];
+                    array[j] = array[i];
+                    array[i] = temp;
                 }
             }
-
         }
-
-        return result;
-
+        return array;
     }
 }
