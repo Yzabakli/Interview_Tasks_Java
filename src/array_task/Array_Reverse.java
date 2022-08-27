d97fa9d404f1bd6b26bd5d526befdd429c90efb3
Yzabakli <abakliyusufziya@gmail.com>
2022-08-27 15:41:13 +0200

    All interview tasks are completed

--- src/array_task/Array_Reverse.java
+++ src/array_task/Array_Reverse.java
@@ -1,11 +1,13 @@
 package array_task;
 
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.stream.Collector;
 
 public class Array_Reverse {
     public static void main(String[] args) {
 
-        System.out.println(Arrays.toString(reverse(new Integer[]{1, 2, 3, 4, 5, 6})));
+        System.out.println(Arrays.toString(reverse(new Object[]{new int[3], 1, "gjdlkfg", 3, 4, 5, 6})));
 
     }
 
@@ -18,5 +20,15 @@ public class Array_Reverse {
             objects[j] = array[i];
         }
         return objects;
+    }
+
+    private static Object[] reverse2(Object[] array) {
+
+        return Arrays.stream(array)
+                .collect(Collector.of(
+                        ArrayDeque::new,
+                        ArrayDeque::addFirst,
+                        (d1, d2) -> { d2.addAll(d1); return d2; }))
+                .toArray();
     }
 }
