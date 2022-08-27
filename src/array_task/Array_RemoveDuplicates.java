0f51bdbd448afe7eed5dc5b3f69c2b6a20cae78a
Yzabakli <abakliyusufziya@gmail.com>
2022-08-27 14:35:57 +0200

    All interview tasks are completed

--- src/array_task/Array_RemoveDuplicates.java
+++ src/array_task/Array_RemoveDuplicates.java
@@ -1,13 +1,12 @@
 package array_task;
 
-import java.util.Arrays;
-import java.util.LinkedHashSet;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class Array_RemoveDuplicates {
     public static void main(String[] args) {
 
-        System.out.println(Arrays.toString(removeDuplicates2(new int[]{1, 1, 2, 2, 3, 3, 4})));
-
+        System.out.println(Arrays.toString(removeDuplicates3_4(new int[]{1, 1, 2, 2, 3, 3, 4})));
     }
 
     private static int[] removeDuplicates(int[] array) {
@@ -50,20 +49,96 @@ public class Array_RemoveDuplicates {
         return count;
     }
 
-    private static int[] removeDuplicates2(int[] array) {
+    private static int[] removeDuplicates2(int[] array){
 
-        LinkedHashSet<Integer> set = new LinkedHashSet<>();
+        return Arrays.stream(array).distinct().toArray();
+    }
 
-        for (int num : array) {
-            set.add(num);
+    private static int[] removeDuplicates3(int[] array){
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i : array) {
+
+            if (!list.contains(i)){
+
+                list.add(i);
+            }
+        }
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    private static int[] removeDuplicates3_2(int[] array){
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i : array) {
+
+            if (!list.contains(i)){
+
+                list.add(i);
+            }
+        }
+        array = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            array[i] = list.get(i);
+        }
+        return array;
+    }
+
+    private static int[] removeDuplicates3_3(int[] array){
+
+        Set<Integer> set = new LinkedHashSet<>();
+
+        for (int i : array) {
+
+            set.add(i);
         }
         array = new int[set.size()];
+        int i = 0;
+        for (Integer integer : set) {
 
-        int a = 0;
-        for (Integer i : set) {
-            array[a++] = i;
+            array[i++] = integer;
         }
+        return array;
+    }
 
+    private static int[] removeDuplicates3_4(int[] array){
+
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        for (int i : array) {
+
+            if (!deque.contains(i)){
+
+                deque.push(i);
+            }
+        }
+        array = new int[deque.size()];
+        int i = 0;
+        for (Integer integer : deque) {
+
+            array[i++] = integer;
+        }
+        return array;
+    }
+
+    private static int[] removeDuplicates3_4_2(int[] array){
+
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        for (int i : array) {
+
+            if (!deque.contains(i)){
+
+                deque.push(i);
+            }
+        }
+        array = new int[deque.size()];
+        int i = 0;
+        while (!deque.isEmpty()){
+            array[i++] = deque.pop();
+        }
         return array;
     }
 }
