c761396cab1c963f5b6a7ddabdfc3a80c8824ba5
Yzabakli <abakliyusufziya@gmail.com>
2022-08-29 20:23:18 +0200

    All interview tasks are completed

--- src/number_tasks/Number_Prime.java
+++ src/number_tasks/Number_Prime.java
@@ -4,26 +4,27 @@ public class Number_Prime {
     public static void main(String[] args) {
 
         long start = System.currentTimeMillis();
-        System.out.println(isPrime(1000551285057161L));
+        System.out.println(solution(1078969597));
         long end = System.currentTimeMillis();
         long elapsedTime = end - start;
         System.out.println("elapsedTime = " + elapsedTime);
     }
     private static boolean isPrime(long num){
 
-        if (num < 2) {
+        if (num == 2) {
+            return true;
+        }
+
+        if (num < 2 || num % 2 == 0) {
             return false;
         }
 
-        boolean prime = num == 2 || num == 3 || num == 5 || num == 7;
-
-        boolean prime2 = num % 2 != 0 && num % 3 != 0 && num % 5 != 0 && num % 7 != 0;
-
-        if (prime){
-
-            return true;
-
-        }else return prime2;
+        for (int i = 3; num >= (long) i * i; i+=2) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
     private static boolean solution(long num) {
         if (num < 2) {
