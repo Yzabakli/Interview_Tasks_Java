78ff4222634528d2f94b98e2e27522ad3766dfcf
Yzabakli <abakliyusufziya@gmail.com>
2022-09-08 18:19:05 +0200

    All interview tasks are completed

--- src/array_task/Array_Triples.java
+++ src/array_task/Array_Triples.java
@@ -3,15 +3,21 @@ package array_task;
 public class Array_Triples {
     public static void main(String[] args) {
 
-        System.out.println(noTriples(new int[]{1, 1, 1, 2, 2, 2, 1}));
+        System.out.println(noTriples(new int[]{1, 1, 2, 2, 1}));
 
     }
 
     private static boolean noTriples(int[] array) {
 
         for (int i = 0; i < array.length; i++) {
-            if (array[i] == array[i + 1] && array[i] == array[i + 2]) {
-                return false;
+            try {
+
+                if (array[i] == array[i + 1] && array[i] == array[i + 2]) {
+                    return false;
+                }
+            }catch (ArrayIndexOutOfBoundsException e){
+
+                break;
             }
         }
         return true;
